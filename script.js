7f5d4016a9fd978f5c965f812809aa30598d5e3c
Nasir Khan <37503821+mnkkhan77@users.noreply.github.com>
2025-04-03 14:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function removeColors() {
+	let select = document.getElementById("colorSelect");
+
+	let selectedIndex = select.selectedIndex;
+
+	if(selectedIndex !== -1){
+		select.remove(selectedIndex)
+	}
+}
+
+document.getElementById("removeButton").addEventListener("click", removeColors);
